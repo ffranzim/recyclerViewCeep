e16e428705f5c0d2144f5f63acb8f23cdc3cfcb6
Fernando Franzim <fernando.franzim@bb.com.br>
2019-02-28 15:10:00 -0300

    Fim aula 4 - Add nota na lista

---
+++ app/src/main/java/br/com/franzim/ceep/ui/activity/FormularioNotaActivity.java
@@ -0,0 +1,42 @@
+package br.com.franzim.ceep.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import br.com.franzim.ceep.R;
+import br.com.franzim.ceep.dao.NotaDAO;
+import br.com.franzim.ceep.model.Nota;
+
+public class FormularioNotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_nota);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_formulario_nota_salva, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_formulario_ic_nota_salva) {
+            EditText edNotaTitulo = findViewById(R.id.formulario_nota_titulo);
+            EditText edNotaDescricao = findViewById(R.id.formulario_nota_descricao);
+
+            Nota nota = new Nota(edNotaTitulo.getText().toString(), edNotaDescricao.getText().toString());
+            new NotaDAO().insere(nota);
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/br/com/franzim/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/franzim/ceep/ui/activity/ListaNotasActivity.java
@@ -1,9 +1,12 @@
 package br.com.franzim.ceep.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
 import br.com.franzim.ceep.R;
 import br.com.franzim.ceep.dao.NotaDAO;
@@ -13,15 +16,35 @@ import br.com.franzim.ceep.ui.recyclerview.adapter.ListaNotasAdapter;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
+
+    private static final NotaDAO notaDAO;
+
+    static {
+        notaDAO = new NotaDAO();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
 
-        NotaDAO notaDAO = new NotaDAO();
         populaNotas(notaDAO);
-        setRecyclerView(notaDAO);
 
+        TextView tvInsereNota = findViewById(R.id.lista_notas_insere_nota);
+        tvInsereNota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListaNotasActivity.this, FormularioNotaActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setRecyclerView(notaDAO);
     }
 
     private void populaNotas(NotaDAO notaDAO) {
@@ -37,4 +60,8 @@ public class ListaNotasActivity extends AppCompatActivity {
 //        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 //        rvNotas.setLayoutManager(layoutManager);
     }
+
+
+
+
 }
